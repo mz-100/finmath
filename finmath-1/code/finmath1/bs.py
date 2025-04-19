3743b80a73888f56dcb194b5f645f4639e74efb4
Mikhail Zhitlukhin <mz-100@outlook.com>
2025-04-20 00:26:56 +0900

    S0 вынесена в аргумент функций

--- finmath-1/code/finmath1/bs.py
+++ finmath-1/code/finmath1/bs.py
@@ -1,38 +1,37 @@
 from dataclasses import dataclass
-import math
+from math import *
 import numpy as np
 from scipy.stats import norm
 
 
 @dataclass
 class BlackScholes:
-    s0: float
     sigma: float
     r: float = 0.0
+    q: float = 0.0
 
-    def d1(self, maturity, strike):
-        return (np.log(self.s0/strike) + (self.r + 0.5*self.sigma**2)*maturity) / (self.sigma*np.sqrt(maturity))
+    def _d1(self, s, t, k):
+        return (np.log(s/k) + (self.r + 0.5*self.sigma**2)*t) / (self.sigma*np.sqrt(t))
 
-    def d2(self, maturity, strike):
-        return self.d1(maturity, strike) - self.sigma*np.sqrt(maturity)
+    def _d2(self, s, t, k):
+        return (np.log(s/k) + (self.r - 0.5*self.sigma**2)*t) / (self.sigma*np.sqrt(t))
 
-    def call_price(self, maturity, strike):
-        return (self.s0*norm.cdf(self.d1(maturity, strike)) 
-                - np.exp(-self.r*maturity)*strike*norm.cdf(self.d2(maturity, strike)))
+    def call_price(self, spot, maturity, strike):
+        return (spot*norm.cdf(self._d1(spot, maturity, strike)) 
+                - np.exp(-self.r*maturity)*strike*norm.cdf(self._d2(spot, maturity, strike)))
     
-    def put_price(self, maturity, strike):
-        return (np.exp(-self.r*maturity)*strike*norm.cdf(-self.d2(maturity, strike)) 
-                - self.s0*norm.cdf(-self.d1(maturity, strike)))
+    def put_price(self, spot, maturity, strike):
+        return (np.exp(-self.r*maturity)*strike*norm.cdf(-self._d2(spot, maturity, strike)) 
+                - spot*norm.cdf(-self._d1(spot, maturity, strike)))
 
-    def simulate(self, t, steps, paths, rng=None):
+    def simulate(self, s0, t, steps, paths, rng=None):
         if rng is None:
             rng = np.random.default_rng()
 
         dt = t/steps
-
         dW = rng.normal(loc=(self.r - 0.5*self.sigma**2)*dt,
-                        scale=self.sigma*math.sqrt(dt),
+                        scale=self.sigma*sqrt(dt),
                         size=(steps, paths))
         logS = np.concatenate([np.zeros((1, paths)), np.cumsum(dW, axis=0)])
         
-        return self.s0*np.exp(logS)
+        return s0*np.exp(logS)
--- finmath-1/code/finmath1/crr.py
+++ finmath-1/code/finmath1/crr.py
@@ -4,7 +4,6 @@ from scipy.special import comb
 
 @dataclass
 class CoxRossRubinstein:
-    s0: float
     u: float
     d: float
     r: float = 0.0
@@ -13,48 +12,48 @@ class CoxRossRubinstein:
     def __post_init__(self):
         self.q = (self.r - self.d) / (self.u - self.d)
 
-    def path_indep_price(self, maturity, payoff):
+    def path_indep_price(self, spot, maturity, payoff):
         N = np.arange(maturity+1)
         Q = comb(maturity, N) * self.q**N * (1-self.q)**N[::-1]
-        S = self.s0 * (1+self.u)**N * (1+self.d)**N[::-1]
+        S = spot * (1+self.u)**N * (1+self.d)**N[::-1]
         return np.dot(Q, payoff(S)) / (1+self.r)**maturity
         
-    def call_price(self, maturity, strike):
+    def call_price(self, spot, maturity, strike):
         payoff = lambda s: np.maximum(np.subtract.outer(s, strike), 0)
-        return self.path_indep_price(maturity, payoff)
+        return self.path_indep_price(spot, maturity, payoff)
     
-    def put_price(self, maturity, strike):
+    def put_price(self, spot, maturity, strike):
         payoff = lambda s: np.maximum(-np.subtract.outer(s, strike), 0)
-        return self.path_indep_price(maturity, payoff)
+        return self.path_indep_price(spot, maturity, payoff)
       
-    def path_dep_price(self, maturity, payoff):
+    def path_dep_price(self, spot, maturity, payoff):
         Omega = np.unpackbits(np.arange(2**maturity, dtype='<u8').view('u1').\
             reshape((-1, 8)), axis=1, count=maturity, bitorder='little').T
         Q = (self.q/(1-self.q))**np.sum(Omega, axis=0) * (1-self.q)**maturity
         xi = np.where(Omega, 1+self.u, 1+self.d)
-        S = self.s0*np.vstack((np.ones(2**maturity), np.cumprod(xi, axis=0)))
+        S = spot * np.vstack((np.ones(2**maturity), np.cumprod(xi, axis=0)))
         X = payoff(S)
         return np.dot(Q, X) / (1+self.r)**maturity
     
-    def simulate(self, t, paths, rng=None):
-        if rng is None:
-            rng = np.random.default_rng()        
-        xi = rng.choice(a=(1+self.u, 1+self.d), p=(self.q, 1-self.q), size=(t,paths))
-        return self.s0*np.vstack([np.ones((1,paths)), xi.cumprod(axis=0)])
-    
-    def american_price(self, maturity, payoff):
+    def american_price(self, spot, maturity, payoff):
         def X(t):
             N = np.arange(t+1)
-            return payoff(self.s0 * (1+self.u)**N * (1+self.d)**(t-N))
+            return payoff(spot * (1+self.u)**N * (1+self.d)**(t-N))
         V = X(maturity)
         for t in range(maturity-1, -1, -1):
             V[:t+1] = np.maximum(X(t), (self.q*V[1:t+2] + (1-self.q)*V[:t+1])/(1+self.r))
         return V[0]
     
-    def american_call_price(self, maturity, strike):
-        return self.american_price(maturity, lambda s: np.maximum(s-strike, 0))
+    def american_call_price(self, spot, maturity, strike):
+        return self.american_price(spot, maturity, lambda s: np.maximum(s-strike, 0))
     
-    def american_put_price(self, maturity, strike):
-        return self.american_price(maturity, lambda s: np.maximum(strike-s, 0))
+    def american_put_price(self, spot, maturity, strike):
+        return self.american_price(spot, maturity, lambda s: np.maximum(strike-s, 0))
 
+    def simulate(self, s0, t, paths, rng=None):
+        if rng is None:
+            rng = np.random.default_rng()  
+        xi = np.where(rng.uniform(size=(t, paths)) < self.q, 1+self.u, 1+self.d)
+        return s0*np.vstack((np.ones(paths), np.cumprod(xi, axis=0)))
+    
     
